0d6894196d5fbd48968133924fae35e4420491d0
Armand Roelens <armand.roelens@connexta.com>
2016-09-19 14:40:15 -0700

    DDF-2478 Add service to convert country codes from FIPS 10-4 to ISO 3166-1 alpha 3 formats and vice versa

--- platform/platform-migratable/src/main/java/org/codice/ddf/platform/migratable/impl/PlatformMigratable.java
+++ platform/platform-migratable/src/main/java/org/codice/ddf/platform/migratable/impl/PlatformMigratable.java
@@ -59,6 +59,8 @@ public class PlatformMigratable extends DescribableBean implements Configuration
     private static final Path APPLICATION_LIST = Paths.get("etc",
             "org.codice.ddf.admin.applicationlist.properties");
 
+    private static final Path FIPS_TO_ISO3 = Paths.get("etc", "fipsToIso3.properties");
+
     private final MigratableUtil migratableUtil;
 
     public PlatformMigratable(@NotNull DescribableBean info,
@@ -89,6 +91,7 @@ public class PlatformMigratable extends DescribableBean implements Configuration
         migratableUtil.copyFile(APPLICATION_LIST, exportDirectory, migrationWarnings);
         migratableUtil.copyFile(DDF_METACARD_ATTRIBUTE_RULESET, exportDirectory, migrationWarnings);
         migratableUtil.copyFile(DDF_USER_ATTRIBUTE_RULESET, exportDirectory, migrationWarnings);
+        migratableUtil.copyFile(FIPS_TO_ISO3, exportDirectory, migrationWarnings);
     }
 
     private void exportWsSecurity(Path exportDirectory,
